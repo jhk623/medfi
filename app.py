6ec21ba6de5f79e61ac89af443d4e15a157d4833
Jaehong Kim <jhk623@gmail.com>
2020-05-27 15:24:08 +0900

    modified:   .idea/workspace.xml 	modified:   __pycache__/app.cpython-38.pyc 	modified:   app.py 	modified:   templates/depression_state.html

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, session, url_for, redirect, \
-    render_template, g, flash, Blueprint
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -8,19 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('init.html', flag=None)
-
-
-@app.route('/test')
-def test():
-    from datetime import time
-    legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('chart.html', values=values, labels=labels, legend=legend)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 @app.route('/state')
